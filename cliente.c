2b577e2bb3257d729e7258c4d11468ef997820d9
Igor Moreira <igormoreirafaria@gmail.com>
2018-10-25 20:34:42 -0300

    meio que foi

--- cliente.c
+++ cliente.c
@@ -1,5 +1,60 @@
-#include <stdio.h>
-int main(int argc, char const *argv[]){
-	
-	return 0;
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+ 
+int main(int argc , char *argv[]){
+    
+	int sock;
+    struct sockaddr_in server;
+    char message[1000] , server_reply[2000];
+
+    //Cria um socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("nao foi possivel criar o socket");
+    }
+    puts("Socket criado");
+     
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8080 );
+
+    //conectar a um servidor
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("Erro, nao foi possivel conectar ao servidor.");
+        return 1;
+    }
+     
+    puts("Connected\n");
+
+    //continuar comunicacao com o servidor
+    while(1)
+    {
+        printf("Enter message : ");
+        scanf("%s" , message);
+         
+        //envia dados
+        if( send(sock , message , strlen(message) , 0) < 0)
+        {
+            puts("falha ao enviar");
+            return 1;
+        }
+         
+        //recebe respostas do servidor
+        if( recv(sock , server_reply , 2000 , 0) < 0)
+        {
+            puts("falha ao receber");
+            break;
+        }
+         
+        puts("resposta do servidor :");
+        puts(server_reply);
+    }
+     
+    //close(sock);
+
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,9 +1,69 @@
 #include "server.h"
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+ 
+int main(int argc , char *argv[]){
+   
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[2000];
 
+    //cria um socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1){
+        printf("nao foi possivel criar o socket");
+    }
+    puts("Socket criado");
 
-int main(int argc, char const *argv[]){
-	
+    //prepara a struct sockaddr_in
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8080 );
 
+    //liga o socket ao servidor
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0){
+        //print the error message
+        perror("Erro, nao foi possivel ligar o socket ao servidor.");
+        return 1;
+    }
+    puts("Ligacao feita");
 
-	return 0;
+    //servidor fica escutando no socket
+    listen(socket_desc , 3);
+
+    //aceita conexões
+    puts("Esperando conexões futuras");
+    c = sizeof(struct sockaddr_in);
+
+    //aceita conexão de um cliente futuro
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("conexao falhou");
+        return 1;
+    }
+    puts("Conexao aceita");
+
+    //recebe mensagem do cliente
+    while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
+    {
+        //retorna mensagem pro cliente
+        write(client_sock , client_message , strlen(client_message));
+        write(client_sock , client_message , 1);
+    }
+     
+    if(read_size == 0)
+    {
+        puts("Cliente disconectou");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("falha ao receber");
+    }
+
+    return 0;
 }
