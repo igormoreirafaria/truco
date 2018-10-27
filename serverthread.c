15e79041fff3975d24d409ab16ef6b82acc6d78d
AnnaAguilar <34386951+AnnaAguilar@users.noreply.github.com>
2018-10-27 16:51:28 -0300

    thread
    thread

--- serverthread.c
+++ serverthread.c
@@ -28,7 +28,7 @@ void * thread_enviada(void * arg) {
                 exit(1);
             }
             ultima_enviada[jogador].valor = '-';
-            ultima_recebida[jogador].naipe = '-';        }
+            ultima_enviada[jogador].naipe = '-';        }
 	}
     close(conexao[jogador]);
 	return NULL;
@@ -50,6 +50,14 @@ void * thread_recebida(void * arg){
 	return NULL;
 }
 
+/*int maior(carta carta1, carta carta2){
+    if(carta1.valor = 4 && carta1.naipe = p) return 1;
+    if(carta2.valor = 4 && carta2.naipe = p) return 2;
+
+    if(carta1.valor = 4 && carta1.naipe = p) return 1;
+    if(carta2.valor = 4 && carta2.naipe = p) return 2;
+
+}*/
 
 
 int main(void) {
@@ -79,8 +87,9 @@ int main(void) {
 
 
     int turno_atual = 0;
+    int pontuacao1 = 0, pontuacao2 = 0;
     //jogo
-    for(;;){
+    while(pontuacao1 < 12 || pontuacao2 < 12){
         if(atual[turno_atual].valor != '-'){
             for(int i = 0; i < 4; i++){
                 ultima_enviada[i].valor = atual[turno_atual].valor;
