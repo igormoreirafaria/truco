7227c14db540f7a3c41367d9b4e08bbd6a8c2fe0
Igor Moreira <igormoreirafaria@gmail.com>
2018-10-28 17:39:16 -0300

    cartas e turnos

--- clientethread.c
+++ clientethread.c
@@ -7,18 +7,27 @@
 #include<arpa/inet.h> //inet_addr
 #include<unistd.h>
 #include<stdlib.h>
+
+typedef struct {
+    char valor[2];
+    char naipe[2];
+}Carta;
+
+Carta *cartas;
+
 int main(int argc , char *argv[])
 {
     int sock;
     struct sockaddr_in server;
-    char *message , server_reply[2000];
+    char *message , *server_reply;
+    server_reply = calloc(sizeof(char), 2000);
     message = malloc(1000 * sizeof(char));
     char *id = malloc(3 * sizeof(char));
     printf("digite id de jogador: \n");
     fgets(id, 3, stdin);
 
     int id_jogador = atoi(id);
-
+    cartas = (Carta*)malloc(3 * sizeof(Carta));
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1)
@@ -40,9 +49,19 @@ int main(int argc , char *argv[])
      
     puts("Connected\n");
      
+    int primeiro_turno = 1;
     //keep communicating with server
     while(1)
     {
+        if( (recv(sock , server_reply , 2000 , 0) < 0) && primeiro_turno == 1){
+            puts("recv failed");
+            break;
+        }
+        puts("cartas: ");
+        puts(server_reply);
+        server_reply = calloc(sizeof(char), 2000);
+
+
         
         for(;id_jogador > 0; id_jogador --) {
             //Receive a reply from the server
@@ -52,7 +71,7 @@ int main(int argc , char *argv[])
             }
             puts("Server reply :");
             puts(server_reply);
-
+            server_reply = calloc(sizeof(char), 2000);
         }
 
 
@@ -73,7 +92,8 @@ int main(int argc , char *argv[])
         //     break;
         // }
          
-        id_jogador = 4;
+        id_jogador = 3;
+        primeiro_turno = 0;
     }
      
     close(sock);
--- serverthread.c
+++ serverthread.c
@@ -9,12 +9,21 @@
 #include<arpa/inet.h> //inet_addr
 #include<unistd.h>    //write
 #include<pthread.h> //for threading , link with lpthread
+#include <time.h>
+
+typedef struct{
+    char valor;
+    char naipe;
+}Carta;
  
 //the thread function
 void *connection_handler(void *);
 
+int check_recursivo(Carta *baralho);
+
 int client_sock[4];
- 
+Carta *baralho;
+
 int main(int argc , char *argv[])
 {
     int socket_desc , c , *new_sock;
@@ -54,6 +63,74 @@ int main(int argc , char *argv[])
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
     int i = 0;
+
+    baralho = (Carta*)malloc(12 * sizeof(Carta));
+
+
+
+    srand(time(NULL));   // Initialization, should only be called once.
+
+    for( int j = 0 ; j < 12 ; j++ ){
+        baralho[j].valor =  48 + ( rand() % 9 );
+    }
+
+    for( int j = 0 ; j < 12 ; j++ ){
+        
+        if( baralho[j].valor  == '1' ) baralho[j].valor = 'A';
+        if( baralho[j].valor == '8' ) baralho[j].valor = 'J';
+        if( baralho[j].valor == '9' ) baralho[j].valor = 'K';
+        if( baralho[j].valor == '0' ) baralho[j].valor = 'Q';
+    }
+
+    for( int j = 0 ; j < 12 ; j++ ){
+        baralho[j].naipe =  97 + ( rand() % 4 );
+    }
+
+    for( int j = 0 ; j < 12 ; j++ ){
+        if( baralho[j].naipe == 'a' ) baralho[j].naipe = 'p';
+        if( baralho[j].naipe == 'b' ) baralho[j].naipe = 'e';
+        if( baralho[j].naipe == 'd' ) baralho[j].naipe = 'o';
+    }
+    int pode_seguir = 1;
+    while(pode_seguir == 1){
+        for(int i = 0 ; i < 12 ; i++ ){
+
+
+            for( int j = i+1 ; j < 12 ; j++ ){
+                
+                if( baralho[j].valor == baralho[i].valor ){
+                    if( baralho[j].naipe == baralho[i].naipe ){
+
+                        if( baralho[j].naipe == 'p' || baralho[j].naipe == 'c' || baralho[j].naipe == 'e') {
+                            baralho[j].naipe = 'o';
+                            printf("chegou 1\n");
+                            break;
+                        }
+                        if( baralho[j].naipe == 'c' || baralho[j].naipe == 'e' || baralho[j].naipe == 'o'){
+                            baralho[j].naipe = 'p';
+                            printf("chegou 2\n");
+                            break;  
+                        } 
+                        if( baralho[j].naipe == 'e' || baralho[j].naipe == 'o' || baralho[j].naipe == 'p'){
+                            baralho[j].naipe = 'e';
+                            printf("chegou 3\n");
+                            break;  
+                        } 
+                        if( baralho[j].naipe == 'o' || baralho[j].naipe == 'p' || baralho[j].naipe == 'e'){
+                            baralho[j].naipe = 'c';
+                            printf("chegou 4\n");
+                            break;  
+                        } 
+
+                    }
+                }
+            }
+        }
+        pode_seguir = check_recursivo(baralho);
+        
+    }
+
+
     while(1)
     {
         if( client_sock[i] = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c) ){
@@ -89,23 +166,79 @@ int main(int argc , char *argv[])
 /*
  * This will handle connection for each client
  * */
+int check_recursivo(Carta *baralho){
+    for(int i = 0 ; i < 12 ; i++ ){
+
+        Carta carta = baralho[i];
+
+        for( int j = i+1 ; j < 12 ; j++ ){
+            
+            if( baralho[j].valor == carta.valor ){
+                if( baralho[j].naipe == carta.naipe ){
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+
 void *connection_handler(void *socket_desc)
 {
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
     int read_size;
-    char *message , client_message[2000];
-     
+    char *message , *client_message, **client_cards;
+    client_message = calloc(sizeof(char), 2000);
+    client_cards = malloc(4 * sizeof(char*));
+    for(int i = 0 ; i < 4 ; i++){
+        client_cards[i] =  malloc(2000 * sizeof(char));
+    }
+    
+
+    sprintf(client_cards[0], " |carta %c %c| |carta %c %c|| |carta %c %c|| ", baralho[0].valor, baralho[0].naipe,
+                                                                              baralho[1].valor, baralho[1].naipe,
+                                                                              baralho[2].valor, baralho[2].naipe);
+
+    sprintf(client_cards[1], " |carta %c %c| |carta %c %c|| |carta %c %c|| ", baralho[3].valor, baralho[3].naipe,
+                                                                              baralho[4].valor, baralho[4].naipe,
+                                                                              baralho[5].valor, baralho[5].naipe);
+
+    sprintf(client_cards[2], " |carta %c %c| |carta %c %c|| |carta %c %c|| ", baralho[6].valor, baralho[6].naipe,
+                                                                              baralho[7].valor, baralho[7].naipe,
+                                                                              baralho[8].valor, baralho[8].naipe);
+
+    sprintf(client_cards[3], " |carta %c %c| |carta %c %c|| |carta %c %c|| ", baralho[9].valor, baralho[9].naipe,
+                                                                              baralho[10].valor, baralho[10].naipe,
+                                                                              baralho[11].valor, baralho[11].naipe);
+
+    for( int i = 0 ; i < 4 ; i++ ){
+        if( sock == client_sock[i]){
+            write(sock , client_cards[i] , strlen(client_cards[i]));
+        }
+    }
+
+    
+
+    
+    
+    //printf("teste server %s\n", client_message);
+
+
+
+
     //Receive a message from client
     while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
     {
+        //printf("teste 1 server %s\n", client_message);
         //Send the message back to client
         for(int i = 0 ; i < 4 ; i++){
 
             write(client_sock[i] , client_message , strlen(client_message));
         
         }
-        memset(client_message, 0, 2000);
+        client_message = calloc(sizeof(char), 2000);
     }
      
     if(read_size == 0)
