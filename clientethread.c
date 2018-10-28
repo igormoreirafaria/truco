7cd72e8f0f97c3719353426c9008186282377882
AnnaAguilar <34386951+AnnaAguilar@users.noreply.github.com>
2018-10-27 22:45:06 -0300

    funciona +-
    tem que ser int depois char

--- clientethread.c
+++ clientethread.c
@@ -4,8 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
-#include <arpa/inet.h> //inet_addr
-#include <string.h>
+
 typedef struct{
     char valor;
     char naipe;
@@ -15,57 +14,42 @@ int main(int argc , char *argv[]){
 
 	int sock;
     struct sockaddr_in servidor;
-    int id_jogador;
-    char *buf;
-    buf = malloc(4 * sizeof(char));
-
-    printf("Qual seu id de jogador: ");
-    scanf("%d", &id_jogador);
-    //Cria um socket
-
+    int id_jogador = atoi(argv[2]);
+    char buf[2];
 
     sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1){
-        printf("nao foi possivel criar o socket");
-    }
-    printf("Socket criado\n");
-
-
+    if (sock == -1) return 1;
 
     servidor.sin_addr.s_addr = inet_addr("127.0.0.1");
     servidor.sin_family = AF_INET;
-    servidor.sin_port = htons( 8081 );
+    servidor.sin_port = htons(atoi(argv[1]));
 
-    //conectar a um servidor
-    if (connect(sock , (struct sockaddr *)&servidor , sizeof(servidor)) < 0) {
-        printf("Erro, nao foi possivel conectar ao servidor.");
-        return 1;
-    }
+    if (connect(sock, (struct sockaddr *)&servidor, sizeof(servidor)) < 0) return 1;
 
     for(;;){
         carta jogada;
-        for(;id_jogador > 0; id_jogador --){
-            recv(sock, buf, 4, 0);
-            
-            printf("%s\n", buf);
+        for(;id_jogador > 0; id_jogador --) {
+            recv(sock, buf, 2, 0);
+            jogada.valor = buf[0];
+            jogada.naipe = buf[1];
+            printf("%c %c \n", jogada.valor, jogada.naipe);
         }
-        printf("Digite sua carta: ");
-        fgets(buf, 4, stdin);
-        buf[strlen(buf)] = '\0';
-        fgets(buf, 4, stdin);
-        buf[strlen(buf)] = '\0';
 
-        send(sock, buf, 4, 0);
+        printf("Digite sua carta\n");
+        scanf("%c %c", &jogada.valor, &jogada.naipe);
+        buf[0] = jogada.valor;
+        buf[1] = jogada.naipe;
 
-        recv(sock, buf, 4, 0);
-   
-        printf("Sua carta: %s\n", buf);
+        printf("enviando: %c %c\n", jogada.valor, jogada.naipe);
+
+        send(sock, buf, 2, 0);
+
+        recv(sock, buf, 2, 0);
+        jogada.valor = buf[0];
+        jogada.naipe = buf[1];
+        printf("Sua carta: %c %c \n", jogada.valor, jogada.naipe);
 
         id_jogador = 3;
     }
-
-    //continuar comunicacao com o servidor
-
-
     return 0;
 }
--- serverthread.c
+++ serverthread.c
@@ -2,137 +2,111 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <string.h>
+#include <netinet/in.h>
 #include <pthread.h>
-#include <arpa/inet.h> //inet_addr
+
 typedef struct{
-    char buf[4];
     char valor;
     char naipe;
 }carta;
 
 carta atual[4]; //carta a ser recebida
 carta ultima_enviada[4]; //ultima carta recebida
-carta ultima_recebida[4]; //ultima carta recebida
 intptr_t conexao[4]; //jogador
 
 void * thread_enviada(void * arg) {
-	int jogador = *((int *) arg);
-	char buf[4];
-	for (;;) {
-		if(ultima_enviada[jogador].valor != '-'){
-            strcpy(buf, ultima_enviada[jogador].buf);
-            // buf[0] = ultima_enviada[jogador].valor;
-            // buf[2] = ultima_enviada[jogador].naipe;
+  int jogador = *((int *) arg);
+  char buf[2];
+  for (;;) {
+    if(ultima_enviada[jogador].valor != '-'){
+      printf("(%d) regindo: %d %c\n", jogador, ultima_enviada[jogador].valor, ultima_enviada[jogador].valor);
 
-            int enviado = send(conexao[jogador], buf, 4, 0);
-            if(enviado != 2){
-                printf("ERRO\n");
-                exit(1);
-            }
-            strcpy(ultima_enviada[jogador].buf, " ");
-            ultima_enviada[jogador].valor = '-';
-            ultima_enviada[jogador].naipe = '-';        }
-	}
-    close(conexao[jogador]);
-	return NULL;
+      buf[0] = ultima_enviada[jogador].valor;
+      buf[1] = ultima_enviada[jogador].naipe;
+
+      int enviado = send(conexao[jogador], buf, 2, 0);
+      printf("(%d) enviado: %d %c\n", jogador, buf[0], buf[1]);
+      if(enviado != 2){
+        printf("ERRO\n");
+        exit(1);
+      }
+
+      ultima_enviada[jogador].valor = '-';
+      ultima_enviada[jogador].naipe = '-';
+    }
+  }
+
+  close(conexao[jogador]);
+  return NULL;
 }
 
 void * thread_recebida(void * arg){
-	int jogador = *((int *) arg);
-	char buf[4];
-	for (;;){
-        int recebido = recv(conexao[jogador], buf, 4, 0);
-        if(recebido < 0){
-            printf("ERRO\n");
-            exit(1);
-        }
-        strcpy(atual[jogador].buf, buf);
-        atual[jogador].valor = buf[0];
-        atual[jogador].naipe = buf[2];
+  int jogador = *((int *) arg);
+  char buf[2];
+  for (;;){
+    int recebido = recv(conexao[jogador], buf, 2, 0);
+    printf("(%d) recebido: %d %c\n", jogador, buf[0], buf[1]);
+    if(recebido < 0){
+      printf("ERRO\n");
+      exit(1);
     }
-	close(conexao[jogador]);
-	return NULL;
+
+    atual[jogador].valor = buf[0];
+    atual[jogador].naipe = buf[1];
+  }
+  close(conexao[jogador]);
+  return NULL;
 }
 
-/*int maior(carta carta1, carta carta2){
-    if(carta1.valor = 4 && carta1.naipe = p) return 1;
-    if(carta2.valor = 4 && carta2.naipe = p) return 2;
 
-    if(carta1.valor = 4 && carta1.naipe = p) return 1;
-    if(carta2.valor = 4 && carta2.naipe = p) return 2;
+int main(void) {
+  int jogadores[4] = {0, 1, 2, 3};
 
-}*/
+  int escuta = socket(AF_INET, SOCK_STREAM, 0);
 
+  if(escuta < 0) return 1;
+  if(listen(escuta, 100) < 0) return 1;
 
-int main(){
+  struct sockaddr_in endereco;
+  socklen_t tamanho_endereco = sizeof(endereco);
 
-    int jogadores[4] = {0, 1, 2, 3};
+  if (getsockname(escuta, (struct sockaddr *) &endereco, &tamanho_endereco) < 0) return 1;
+  printf("Escutando na porta: %d\n", ntohs(endereco.sin_port));
 
+  for (int i = 0; i < 4; i++) {
+    conexao[i] = accept(escuta, NULL, NULL);
+    pthread_t thread_envia, thread_recebe;
 
+    ultima_enviada[i].naipe = atual[i].naipe = '-';
+    ultima_enviada[i].valor = atual[i].valor = '-';
 
-	int escuta = socket(AF_INET, SOCK_STREAM, 0);
-    if(escuta < 0) return 1;
+    if(pthread_create(&thread_envia, NULL, thread_enviada, (void*) &(jogadores[i])) != 0) return 1;
+    if(pthread_create(&thread_recebe, NULL, thread_recebida, (void*) &(jogadores[i])) != 0) return 1;
+  }
+  printf("oi\n");
 
-    int c;
-    struct sockaddr_in endereco, cliente[4];
+  int turno_atual = 0;
+  for(;;){
 
-    endereco.sin_family = AF_INET;
-    endereco.sin_addr.s_addr = INADDR_ANY;
-    endereco.sin_port = htons( 8081 );
+    if(atual[turno_atual].valor != '-'){
+      printf("mudou: %d %c\n", atual[turno_atual].valor, atual[turno_atual].naipe);
 
-    socklen_t tamanho_endereco = sizeof(endereco);
+      for(int i = 0; i < 4; i++){
+        ultima_enviada[i].valor = atual[turno_atual].valor;
+        ultima_enviada[i].naipe = atual[turno_atual].naipe;
+        printf("(%d) setei: %d %c\n", i, ultima_enviada[i].valor, ultima_enviada[i].naipe);
+      }
 
-   if( bind(escuta,(struct sockaddr *)&endereco , sizeof(endereco)) < 0){
-        //print the error message
-        perror("Erro, nao foi possivel ligar o socket ao servidor.");
-        return 1;
+      atual[turno_atual].naipe = atual[turno_atual].valor = '-';
+      printf("resetei: %d %c\n", atual[turno_atual].valor, atual[turno_atual].naipe);
+
+      for(int i = 0; i < 4; i++){
+        while(ultima_enviada[i].valor != '-');
+      }
+
+      turno_atual = (turno_atual + 1) % 4;
     }
-    printf("Escutando na porta %d\n", htons(endereco.sin_port));
+  }
 
-    if(listen(escuta, 100) < 0) return 1;
-
-
-	for (int i = 0; i < 4; i++) {
-        
-        c = sizeof(struct sockaddr_in);
-        conexao[i] = accept(escuta, (struct sockaddr *)&cliente[i], (socklen_t*)&c);
-        if (conexao[i] < 0){
-            perror("conexao falhou");
-            return 1;
-        }
-        printf("Conexao aceita cliente %d\n", i);
-		pthread_t thread_envia, thread_recebe;
-
-        ultima_enviada[i].naipe = ultima_enviada[i].valor = atual[i].naipe = atual[i].valor = '-';
-
-		if(pthread_create(&thread_envia, NULL, thread_enviada, (void*) &(jogadores[i])) != 0) return 1;
-        if(pthread_create(&thread_recebe, NULL, thread_recebida, (void*) &(jogadores[i])) != 0) return 1;
-	}
-
-
-
-    int turno_atual = 0;
-    int pontuacao1 = 0, pontuacao2 = 0;
-    //jogo
-    while(pontuacao1 < 12 || pontuacao2 < 12){
-        if(atual[turno_atual].valor != '-'){
-            for(int i = 0; i < 4; i++){
-                strcpy(ultima_enviada[i].buf, atual[turno_atual].buf);
-                ultima_enviada[i].valor = atual[turno_atual].valor;
-                ultima_enviada[i].naipe = atual[turno_atual].naipe;
-            }
-            atual[turno_atual].naipe = atual[turno_atual].valor = '-';
-            
-            while(ultima_enviada[0].valor != '-');
-            while(ultima_enviada[1].valor != '-');
-            while(ultima_enviada[2].valor != '-');
-            while(ultima_enviada[3].valor != '-');
-
-            turno_atual = (turno_atual + 1) % 4;
-        }
-    }
-
-    printf("%ld ", sizeof(carta));
-	return 0;
+  return 0;
 }
