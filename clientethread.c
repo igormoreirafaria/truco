a6a80bd622f382d22c8a35b172f37dc896851be2
Igor Moreira <igormoreirafaria@gmail.com>
2018-10-28 15:13:45 -0300

    l

--- clientethread.c
+++ clientethread.c
@@ -1,55 +1,81 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <pthread.h>
-#include <arpa/inet.h> //inet_addr
-typedef struct{
-    char valor;
-    char naipe;
-}carta;
+/*
+    C ECHO client example using sockets
+*/
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>
+#include<stdlib.h>
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char *message , server_reply[2000];
+    message = malloc(1000 * sizeof(char));
+    char *id = malloc(3 * sizeof(char));
+    printf("digite id de jogador: \n");
+    fgets(id, 3, stdin);
 
-int main(int argc , char *argv[]){
+    int id_jogador = atoi(id);
 
-	int sock;
-    struct sockaddr_in servidor;
-    int id_jogador = atoi(argv[2]);
-    char buf[2];
-
+    //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1) return 1;
-
-    servidor.sin_addr.s_addr = inet_addr("127.0.0.1");
-    servidor.sin_family = AF_INET;
-    servidor.sin_port = htons(8080);
-
-    if (connect(sock, (struct sockaddr *)&servidor, sizeof(servidor)) < 0) return 1;
-
-    for(;;){
-        carta jogada;
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8080 );
+ 
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+     
+    puts("Connected\n");
+     
+    //keep communicating with server
+    while(1)
+    {
+        
         for(;id_jogador > 0; id_jogador --) {
-            recv(sock, buf, 2, 0);
-            jogada.valor = buf[0];
-            jogada.naipe = buf[1];
-            printf("%c %c \n", jogada.valor, jogada.naipe);
+            //Receive a reply from the server
+            if( recv(sock , server_reply , 2000 , 0) < 0){
+                puts("recv failed");
+                break;
+            }
+            puts("Server reply :");
+            puts(server_reply);
+
         }
 
-        printf("Digite sua carta\n");
-        scanf("%c %c", &jogada.valor, &jogada.naipe);
-        buf[0] = jogada.valor;
-        buf[1] = jogada.naipe;
 
-        printf("enviando: %c %c\n", jogada.valor, jogada.naipe);
-
-        send(sock, buf, 2, 0);
-
-        recv(sock, buf, 2, 0);
-        jogada.valor = buf[0];
-        jogada.naipe = buf[1];
-        printf("Sua carta: %c %c \n", jogada.valor, jogada.naipe);
-
-        id_jogador = 3;
+        printf("Enter message : ");
+        fgets(message, 1000, stdin );
+        
+        //Send some data
+        if( send(sock , message , strlen(message) , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+         
+        // //Receive a reply from the server
+        // if( recv(sock , server_reply , 2000 , 0) < 0)
+        // {
+        //     puts("recv failed");
+        //     break;
+        // }
+         
+        id_jogador = 4;
     }
+     
+    close(sock);
     return 0;
 }
--- serverthread.c
+++ serverthread.c
@@ -1,130 +1,125 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <pthread.h>
-#include <arpa/inet.h> //inet_addr
+/*
+    C socket server example, handles multiple clients using threads
+*/
+ 
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<stdlib.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include<pthread.h> //for threading , link with lpthread
+ 
+//the thread function
+void *connection_handler(void *);
 
-
-typedef struct{
-    char valor;
-    char naipe;
-}carta;
-
-carta atual[4]; //carta a ser recebida
-carta ultima_enviada[4]; //ultima carta recebida
-intptr_t conexao[4]; //jogador
-
-void * thread_enviada(void * arg) {
-  int jogador = *((int *) arg);
-  char buf[2];
-  for (;;) {
-    if(ultima_enviada[jogador].valor != '-'){
-      printf("(%d) regindo: %d %c\n", jogador, ultima_enviada[jogador].valor, ultima_enviada[jogador].valor);
-
-      buf[0] = ultima_enviada[jogador].valor;
-      buf[1] = ultima_enviada[jogador].naipe;
-
-      int enviado = send(conexao[jogador], buf, 2, 0);
-      printf("(%d) enviado: %d %c\n", jogador, buf[0], buf[1]);
-      if(enviado != 2){
-        printf("ERRO\n");
-        exit(1);
-      }
-
-      ultima_enviada[jogador].valor = '-';
-      ultima_enviada[jogador].naipe = '-';
+int client_sock[4];
+ 
+int main(int argc , char *argv[])
+{
+    int socket_desc , c , *new_sock;
+    struct sockaddr_in server , client;
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
     }
-  }
-
-  close(conexao[jogador]);
-  return NULL;
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8080 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(socket_desc , 3);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+     
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+    int i = 0;
+    while(1)
+    {
+        if( client_sock[i] = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c) ){
+            puts("Connection accepted");
+        } 
+         
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock[i];
+         
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+         
+        //Now join the thread , so that we dont terminate before the thread
+        //pthread_join( sniffer_thread , NULL);
+        puts("Handler assigned");
+        i++;
+        if( i == 5 ) i = 0;
+    }
+     
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+     
+    return 0;
 }
+ 
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket_desc;
+    int read_size;
+    char *message , client_message[2000];
+     
+    //Receive a message from client
+    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
+    {
+        //Send the message back to client
+        for(int i = 0 ; i < 4 ; i++){
 
-void * thread_recebida(void * arg){
-  int jogador = *((int *) arg);
-  char buf[2];
-  for (;;){
-    int recebido = recv(conexao[jogador], buf, 2, 0);
-    printf("(%d) recebido: %d %c\n", jogador, buf[0], buf[1]);
-    if(recebido < 0){
-      printf("ERRO\n");
-      exit(1);
+            write(client_sock[i] , client_message , strlen(client_message));
+        
+        }
+        memset(client_message, 0, 2000);
     }
-
-    atual[jogador].valor = buf[0];
-    atual[jogador].naipe = buf[1];
-  }
-  close(conexao[jogador]);
-  return NULL;
+     
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+         
+    //Free the socket pointer
+    free(socket_desc);
+     
+    return 0;
 }
-
-
-int main(void) {
-  int jogadores[4] = {0, 1, 2, 3};
-
-  int escuta = socket(AF_INET, SOCK_STREAM, 0);
-  if(escuta < 0){
-    printf("erro  ao criar socket\n");
-    return 1;
-  }
-  printf("socket criado\n");
-
-  struct sockaddr_in endereco;
-  socklen_t tamanho_endereco = sizeof(endereco);
-
-  endereco.sin_addr.s_addr = inet_addr("127.0.0.1");
-  endereco.sin_family = AF_INET;
-  endereco.sin_port = htons(8080);
-
-
-
-
-  if (bind(escuta, (struct sockaddr *) &endereco, (socklen_t)tamanho_endereco) < 0) {
-    return 1;
-  }
-  printf("bind feito\n");
-
-  if(listen(escuta, 100) < 0){
-    printf("erro ao executar listen\n");
-    return 1;
-  } 
-  printf("Escutando na porta: %d\n", htons(endereco.sin_port));
-  for (int i = 0; i < 4; i++) {
-    conexao[i] = accept(escuta, NULL, NULL);
-    pthread_t thread_envia, thread_recebe;
-
-    ultima_enviada[i].naipe = atual[i].naipe = '-';
-    ultima_enviada[i].valor = atual[i].valor = '-';
-
-    if(pthread_create(&thread_envia, NULL, thread_enviada, (void*) &(jogadores[i])) != 0) return 1;
-    if(pthread_create(&thread_recebe, NULL, thread_recebida, (void*) &(jogadores[i])) != 0) return 1;
-  }
-
-
-  int turno_atual = 0;
-  for(;;){
-
-    if(atual[turno_atual].valor != '-'){
-      printf("mudou: %d %c\n", atual[turno_atual].valor, atual[turno_atual].naipe);
-
-      for(int i = 0; i < 4; i++){
-        ultima_enviada[i].valor = atual[turno_atual].valor;
-        ultima_enviada[i].naipe = atual[turno_atual].naipe;
-        printf("(%d) setei: %d %c\n", i, ultima_enviada[i].valor, ultima_enviada[i].naipe);
-      }
-
-      atual[turno_atual].naipe = atual[turno_atual].valor = '-';
-      printf("resetei: %d %c\n", atual[turno_atual].valor, atual[turno_atual].naipe);
-
-      for(int i = 0; i < 4; i++){
-        while(ultima_enviada[i].valor != '-');
-      }
-
-      turno_atual = (turno_atual + 1) % 4;
-    }
-  }
-
-  return 0;
-  }
